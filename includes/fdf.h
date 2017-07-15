481ac06c1866036fbe2e3543176746eec1fad726
Pema Horeau <phoreau@e1z3r5p12.42.us.org>
2017-07-14 18:46:27 -0700

    need to start drawing

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: phoreau <phoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/09 14:57:35 by phoreau           #+#    #+#             */
-/*   Updated: 2017/07/14 15:21:12 by phoreau          ###   ########.fr       */
+/*   Updated: 2017/07/14 18:27:06 by phoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,33 @@
 # include "../libft/libft.h"
 # include "../minilibx_macos/mlx.h"
 
+# define KEY_ESCAPE	53
+# define KEY_ENTER	36
+# define KEY_LEFT	123
+# define KEY_DOWN	125
+# define KEY_RIGHT	124
+# define KEY_UP		126
+
+typedef struct		s_points
+{
+	float			x;
+	float			y;
+	float			z;
+}					t_points;
+
 typedef struct		s_env
 {
 	void			*mlx;
 	void			*win;
-					// gap;
-					// m;
-					
-					// cart;
-					// j;
-					// i;
-
+	float			y1;
+	float			x1;
+	float			x2;
+	float			y2;
+	int				hmin;
+	int				hmax;
+	int				w;
+	int				h;
+	t_points		**points;
 }					t_env;
 
 typedef struct		s_map
@@ -43,12 +59,13 @@ typedef struct		s_map
 	int				mid_y;
 }					t_map;
 
-typedef struct		s_points
+typedef struct		s_info
 {
-	float			x;
-	float			y;
-	float			z;
-}					t_points;
+	int				numlines;
+	int				numcols;
+	int				height;
+	int				width;
+}					t_info;
 
 //		Parsing
 int					parse(char *argument);
@@ -56,9 +73,8 @@ void				error(int x);
 
 //		Storing
 int					find_width(char **coord);
-void				find_width_heigth(int fd, t_map *in_map);
+void				find_width_height_and_z(int fd, t_map *in_map);
 void				storing(char *argument, t_map *in_map);
-void				find_z_value(int fd, t_map *in_map);
 
 //		Bullshit
 void				linebres_negative(int x0, int y0, int x1, int y1);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: phoreau <phoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/09 14:06:45 by phoreau           #+#    #+#             */
-/*   Updated: 2017/07/14 15:20:47 by phoreau          ###   ########.fr       */
+/*   Updated: 2017/07/14 17:25:26 by phoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,18 @@ void	error(int x)
 		exit(1);
 }
 
+int		key_hook(int keycode, t_env *e)
+{
+	ft_putstr("Keyevent: ");
+	ft_putendl(ft_itoa(keycode));
+	if (keycode == KEY_ESCAPE)
+		exit(0);
+	return (0);
+}
+
 int		main(int ac, char **av)
 {
+	t_env	e;
 	t_map	*in_map;
 
 	if (ac != 2)
@@ -33,5 +43,9 @@ int		main(int ac, char **av)
 		error(0);
 	parse(av[1]);
 	storing(av[1], in_map);
+	e.mlx = mlx_init();
+	e.win = mlx_new_window(e.mlx, 600, 600, "FDF");
+	mlx_key_hook(e.win, key_hook, &e);
+	mlx_loop(e.mlx);
 	return (0);
 }
